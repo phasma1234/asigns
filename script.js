5dc04840411de0206df931f39ec5b5f271a3e1fc
phasma <titolinti@gmail.com>
2025-08-26 19:07:53 +0300

    Username Input

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let count=0;
-const myClick= document.getElementById('myButton');
-const myDisplay=document.getElementById('myCount');
+/*let Count =0;
+const Button=document.getElementById('myButton')
+const Display=document.getElementById('myDisplay')
 
-myClick.addEventListener('click', function(){
-    count++;
-    myDisplay.textContent=count;
+Button.addEventListener('click', function(){
+    Count++;
+    Display.textContent=Count;
+});
 
-})
+const button = document.getElementById("changeColorBtn");
+    const box = document.getElementById("colorBox");
+
+    // function to generate random hex color
+    function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    button.addEventListener("click", function () {
+      box.style.backgroundColor = getRandomColor();
+    });
 
 
+const Greetings=document.getElementById('greetings')
+
+const Username=document.getElementById('username');
+
+Username.addEventListener('input', function(){
+
+  Greetings.textContent='Your Name is ' + Username.value;
+})*/
+
+const Greetings = document.getElementById('greetings');
+const Username = document.getElementById('username');
+
+Username.addEventListener('input', function() {
+  Greetings.textContent = "Your name is " + Username.value;
+});
